92abdd5299b4f2d2df1c691334b1e376bbb35757
Steffen Arntz <steffen.arntz@ucware.de>
2017-04-25 10:54:40 +0200

    stuff to use buttons/switches

--- dew_cool_heat.ino
+++ dew_cool_heat.ino
@@ -1,13 +1,21 @@
 #include <Adafruit_LiquidCrystal.h>
+#include <HashMap.h>
 
 #include <AM2320.h>
 #include <BME280I2C.h>
 
-#define GOVERN_INTERVAL 1000
+#define GOVERN_INTERVAL 100
 #define DISPLAY_INTERVAL 1000
 #define SCREEN_INTERVALL 4000
-#define MOTOR_PIN A1
-#define MOTOR_ENABLE_PIN 1
+#define MOTOR_PIN 1
+#define MOTOR_ENABLE_PIN 2
+#define OFF_SWITCH_PIN 3
+#define HAND_SWITCH_PIN 4
+#define AUTO_SWITCH_PIN 5
+#define TOR_SWITCH_PIN 6
+#define POTI_PIN A0
+
+CreateHashMap(ledPins, String, int, 4);
 
 boolean auto_display = true;
 boolean dirty_display = false;
@@ -17,13 +25,14 @@ unsigned long last_display_refresh = 0;
 unsigned long last_govern = 0;
 boolean motor_running = false;
 byte motor_speed = 0;
+boolean tor_offen = false;
 enum class State : byte {
   off,
   manual,
   automatic
 };
 
-enum Onreason : byte {
+enum class OnReason : byte {
   inactive,
   manual_override,
   dew_point,
@@ -31,7 +40,7 @@ enum Onreason : byte {
   heating
 };
 
-Onreason onreason = inactive;
+OnReason onreason = OnReason::inactive;
 State state = State::off;
 
 class SensorData {
@@ -41,6 +50,9 @@ class SensorData {
     float pres = 0;
     float dew = 0;
 };
+
+
+
 
 SensorData sensord1;
 SensorData sensord2;
@@ -67,7 +79,8 @@ void loop() {
   // put your main code here, to run repeatedly:
   //bme280
   if (millis() - last_govern > GOVERN_INTERVAL) {
-    updateDisplay();
+    checkUI();
+    readSensors();
     governMotor();    
     last_govern = millis();
   }
@@ -157,34 +170,64 @@ void governMotor() {
   if(state != State::automatic){
     return;
   }
-  onreason = inactive;
+  onreason = OnReason::inactive;
   if(sensord1.dew > sensord2.dew && state == State::automatic){
-    onreason = dew_point;
+    onreason = OnReason::dew_point;
   }
-  if(sensord1.temp > sensord2.temp && onreason == dew_point && sensord2.temp < 16){
-    onreason = heating;
+  if(sensord1.temp > sensord2.temp && onreason == OnReason::dew_point && sensord2.temp < 16){
+    onreason = OnReason::heating;
   }
-  if(sensord1.temp < sensord2.temp && onreason == dew_point && sensord2.temp > 22){
-    onreason = heating;
+  if(sensord1.temp < sensord2.temp && onreason == OnReason::dew_point && sensord2.temp > 22){
+    onreason = OnReason::heating;
   }
   switch (onreason){
-    case heating:{
-      setMotorSpeed(true, 255);
+    case OnReason::heating:{
+      setMotorSpeed(255);
     }
-    case cooling:{
-      setMotorSpeed(true, 255);
+    case OnReason::cooling:{
+      setMotorSpeed(255);
     }
-    case dew_point:{
-      setMotorSpeed(true, 64);
+    case OnReason::dew_point:{
+      setMotorSpeed(64);
     }
     default:{
-      setMotorSpeed(false, 0);
+      setMotorSpeed(0);
     }
   }
 }
 
-void setMotorSpeed(boolean set_state, byte set_speed) {
-  analogWrite(MOTOR_PIN, set_speed);
-  digitalWrite(MOTOR_ENABLE_PIN, set_state && state != State::off);
+void setMotorSpeed(byte set_speed) {
+  if(tor_offen){
+    set_speed = set_speed / 2;
+  }
+  if(state == State::manual){
+    set_speed = map(analogRead(POTI_PIN),0,1023,10,255);
+  }
+  if(motor_speed > set_speed) {
+    motor_speed = motor_speed - ((motor_speed - set_speed)/2 - 1);
+  } else if (motor_speed < set_speed) {
+    motor_speed = motor_speed + ((set_speed - motor_speed)/2 + 1);
+  }
+  analogWrite(MOTOR_PIN, motor_speed);
+  digitalWrite(MOTOR_ENABLE_PIN, state != State::off);
+}
+
+void checkUI(){
+  boolean s_off = (digitalRead(OFF_SWITCH_PIN) == LOW);
+  boolean s_hand = (digitalRead(HAND_SWITCH_PIN) == LOW);
+  boolean s_auto = (digitalRead(AUTO_SWITCH_PIN) == LOW);
+  boolean tor_offen = (digitalRead(TOR_SWITCH_PIN) == LOW);
+  if(s_off) {
+    state = State::off;    
+    return;
+  }
+  if(s_hand && !s_off && !s_auto) {
+    state = State::manual;
+    return;
+  }
+  if(s_auto && !s_hand && !s_off) {
+    state = State::automatic;
+  }
+  state = State::off;
 }
 
